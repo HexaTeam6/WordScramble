7b0c578552b5fbdc17a2605d451062521d300c34
HexaTeam6 <abdurrachmanwahed@gmail.com>
2022-04-28 18:33:21 +0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Abdur Rachman Wahed on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
